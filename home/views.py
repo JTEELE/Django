c83b498bba0ba60b4fda5c34e32590c4e4d0a43c
JTEELE <j.teele@outlook.com>
2022-03-20 15:18:39 -0500

    included login and logout endpoints

--- home/views.py
+++ home/views.py
@@ -1,12 +1,20 @@
+from distutils.log import Log
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 # from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+
+class LogoutInterfaceView(LogoutView):
+    template_name='home/logout.html'
 
 # def home(request):
 #     return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+class LoginInterfaceView(LoginView):
+    template_name = 'home/login.html'
 
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
